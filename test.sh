4cdd614c7291a51bfe42c2245fd5b476dbdafca4
exorfer <exorfer@gmail.com>
2019-04-24 12:07:57 +0700

    Help + exit(3)

--- test.sh
+++ test.sh
@@ -29,7 +29,7 @@ test 6 1 -h
 test 7 1 -login foo@example.com
 test 8 2 -login zzz -pass qwerty
 test 9 3 -login xxx@example.com -pass 123456
-#test 10 4 -login foo@example.com -pass abc
+test 10 4 -login foo@example.com -pass abc
 
 if [ $COUNT_ALL -eq $COUNT_SUC ]; then
     echo All $COUNT_ALL tests passed
